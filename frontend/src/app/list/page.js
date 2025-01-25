4b26b9280936d4bc6a277e27dafab0e12861cbe0
Fahad <mofahadkp@proton.me>
2025-01-25 19:58:10 +0530

    Fixed loading error

--- frontend/src/app/list/page.js
+++ frontend/src/app/list/page.js
@@ -1,9 +1,7 @@
-import ListPage from './list_ui';
+import ListPage from "./list_ui";
 
-export default function App() {
-  return (
-    <ListPage />
-  );
+function App() {
+  return <ListPage />;
 }
 
 export default App;
