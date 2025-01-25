31b68c61ed0f91aff666196459296dfd67111d69
Muflih-uk <muhammedmuflih875@gmail.com>
2025-01-25 22:55:48 +0530

    SWR Created

--- frontend/src/app/document/page.js
+++ frontend/src/app/document/page.js
@@ -1,9 +1,19 @@
-import Aadhar from "./aadhar";
+"use client";
 
-export default function Doc() {
+import Aadhar from "./aadhar";
+import axios from 'axios';
+import useSWR from 'swr';
+const fetcher = url => axios.get(url).then(res=>res.data)
+
+export default function DocumentSWR(){
+  const {data,error} = useSWR('http://192.168.177.88:3000/document',fetcher)
+
+  if(error) return console.log(error)
+  if(!data) return console.log("Loading") 
   return (
     <div>
-      <Aadhar />
+      <Aadhar/>
     </div>
-  );
-}
+    );
+  } 
+
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,4 @@
+
 export default function Home() {
   const popularDocs = ["Aadhar", "PAN Card", "Voter ID"];
 
