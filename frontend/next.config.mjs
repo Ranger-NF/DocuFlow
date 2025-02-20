1ccce1d6a5196961cfcec420d99efab999ecde57
Fahad <mofahadkp@proton.me>
2025-02-20 21:25:44 +0530

    Added workflow

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true
+    },
+    basePath: '/DocuFlow', 
+    assetPrefix: '/DocuFlow/',
+  }
+  
+  module.exports = nextConfig
